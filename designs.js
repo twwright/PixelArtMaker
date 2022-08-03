6cd7ca0347b856647e3df155d7a3b5e035537807
twwright <twright@hey.com>
2022-08-03 17:53:02 -0400

    Stubs loop to create grid from array values

--- designs.js
+++ designs.js
@@ -1,15 +1,25 @@
 'use strict';
 
 function clear(canvas) {
-    while (canvas.rows.length !+ 0) {
+    while (canvas.rows.length != 0) {
         canvas.deleteRow(0);
     }
 }
 
-
-
 function makeGrid() {
-    const gridWidth = document.getElementById('inputWidth').value;
-    const gridHeight = document.getElementById('inputHeight').value;
     const canvas = document.getElementById('pixelCanvas');
     clear(canvas);
+
+    const inputWidth = document.getElementById('inputWidth').value;
+    const inputHeight = document.getElementById('inputHeight').value;
+
+    const gridWidth = new Array(inputWidth).fill().map((_, idx) => idx);
+    const gridHeight = new Array(inputHeight).fill().map((_, idx) => idx);
+
+    for (row of gridHeight) {
+        let row = canvas.insertRow(row);
+        for (col of gridWidth) {
+            let newCell = row.insertCell(col);
+    }
+}
+
