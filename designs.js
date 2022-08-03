10bdab30d35bf620081a94bda7494bed04818b64
twwright <twright@hey.com>
2022-08-03 17:49:51 -0400

    Clears grid before making it

--- designs.js
+++ designs.js
@@ -1,11 +1,8 @@
-// When size is submitted by the user, call makeGrid()
-
 function makeGrid() {
     const gridWidth = document.getElementById('inputWidth').value;
     const gridHeight = document.getElementById('inputHeight').value;
     const canvas = document.getElementById('pixelCanvas');
-    
 
-// Your code goes here!
-
-}
+    while (canvas.rows.length != 0) {
+        canvas.deleteRow(0);
+    }
