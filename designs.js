f7972cfeaaecf7b645fb21b910f2d62e3f26f6ab
twwright <twright@hey.com>
2022-08-03 17:50:55 -0400

    Turns on strict mode

--- designs.js
+++ designs.js
@@ -1,12 +1,15 @@
+'use strict';
+
 function clear(canvas) {
     while (canvas.rows.length !+ 0) {
         canvas.deleteRow(0);
     }
 }
 
+
+
 function makeGrid() {
     const gridWidth = document.getElementById('inputWidth').value;
     const gridHeight = document.getElementById('inputHeight').value;
     const canvas = document.getElementById('pixelCanvas');
     clear(canvas);
-    
