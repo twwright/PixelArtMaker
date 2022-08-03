854672250e6107eec47159c1cca5d756ee3dffb3
twwright <twright@hey.com>
2022-08-03 17:48:31 -0400

    Selects initial elements for the makeGrid function

--- designs.js
+++ designs.js
@@ -1,9 +1,10 @@
-// Select color input
-// Select size input
-
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+    const gridWidth = document.getElementById('inputWidth').value;
+    const gridHeight = document.getElementById('inputHeight').value;
+    const canvas = document.getElementById('pixelCanvas');
+    
 
 // Your code goes here!
 
