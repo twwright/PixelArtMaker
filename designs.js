4bf2cac6b8e73b18cad4625bfd6ff5990217a69b
twwright <twright@hey.com>
2022-08-03 17:50:32 -0400

    Extracts clearing the canvas into its own function

--- designs.js
+++ designs.js
@@ -1,8 +1,12 @@
+function clear(canvas) {
+    while (canvas.rows.length !+ 0) {
+        canvas.deleteRow(0);
+    }
+}
+
 function makeGrid() {
     const gridWidth = document.getElementById('inputWidth').value;
     const gridHeight = document.getElementById('inputHeight').value;
     const canvas = document.getElementById('pixelCanvas');
-
-    while (canvas.rows.length != 0) {
-        canvas.deleteRow(0);
-    }
+    clear(canvas);
+    
